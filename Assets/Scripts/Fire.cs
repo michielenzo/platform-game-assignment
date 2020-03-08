3d5361df98d7bf49ba3cf3d0956558b79c69c5c0
Michiel Idema <m.idema@textinfo.nl>
2020-03-08 17:21:55 +0100

    only 3 bullets can be fired at once now

--- Assets/Scripts/Fire.cs
+++ Assets/Scripts/Fire.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class Fire : MonoBehaviour
 {
@@ -7,16 +9,26 @@ public class Fire : MonoBehaviour
 
     public float bulletLifeSpan;
     public float bulletSpeed;
+    public int maxBullets;
 
     public Vector3 bulletSpawnOffset;
     
     private int _bulletsInWorld;
 
-    private bool _fireLock = false;
+    private bool _fireLock;
+
+    private List<GameObject> _bullets;
+
+    public void Awake()
+    {
+        _bullets = new List<GameObject>();
+        _fireLock = false;
+    }
 
     public void Update()
     {
         //You will only fire again if the fire button was released again.
+        // ReSharper disable once CompareOfFloatsByEqualityOperator
         if (Input.GetAxis("Fire") == 1) {
             if (_fireLock) return;
             FireBullet();
@@ -28,8 +40,10 @@ public class Fire : MonoBehaviour
 
     private void FireBullet()
     {
+        if (_bullets.Count >= maxBullets) return;
         Vector3 direction = transform.forward.normalized;
         GameObject bullet = Instantiate(bulletObj, transform.position + bulletSpawnOffset, Quaternion.identity);
+        _bullets.Add(bullet);
         bullet.gameObject.GetComponent<Rigidbody>().AddForce(direction * (bulletSpeed * Time.deltaTime));
     }
 }
