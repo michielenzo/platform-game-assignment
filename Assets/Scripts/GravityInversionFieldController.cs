b061f0b7a5e8e93572c55c0de7ee8d075f84e4e5
michielenzo <michielidema@outlook.com>
2020-03-24 16:32:58 +0100

    gravity inversion field works now.

---
+++ Assets/Scripts/GravityInversionFieldController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityInversionFieldController : MonoBehaviour
+{
+
+    private List<Rigidbody> _rigidBodies;
+
+    private Vector3 _gravityDirection;
+
+    public float _forceMultiplier;
+
+    private void Awake()
+    {
+        _rigidBodies = new List<Rigidbody>();
+        _gravityDirection = Vector3.up;
+    }
+
+    private void FixedUpdate()
+    {
+        foreach (var rigidBody in _rigidBodies) {
+            if (rigidBody == null) {
+                _rigidBodies.Remove(rigidBody);
+                return;
+            }
+            
+            InvertGravity(rigidBody);
+        }
+    }
+
+    private void InvertGravity(Rigidbody rigidBody)
+    {
+        rigidBody.AddForce(_gravityDirection * (_forceMultiplier * Time.deltaTime));
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Rigidbody rigidBody = other.GetComponent<Rigidbody>();
+        if (rigidBody == null) return;
+        rigidBody.useGravity = false;
+        _rigidBodies.Add(rigidBody);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Rigidbody rigidBody = other.GetComponent<Rigidbody>();
+        if (rigidBody == null) return;
+        rigidBody.useGravity = true;
+        _rigidBodies.Remove(rigidBody);
+    }
+}
