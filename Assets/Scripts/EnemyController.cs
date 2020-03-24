acd12fbb2d9a03cf13047e6553f4bd40e5fc5c70
michielenzo <michielidema@outlook.com>
2020-03-24 17:49:08 +0100

    Enemy can spawn gravityinverionfields now.

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -2,12 +2,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 public class EnemyController : MonoBehaviour
 {
     public EnemyConfig _EnemyConfig;
     public GameObject playerObject;
+
+    public GameObject gravityInversionFieldReference;
+    
     private Rigidbody _rigidBody;
+
+    private bool _summonGravityInversionFieldLock;
+    public int _summoningReloadTime;
+    public int gravityInversionFieldLifetime;
 
     public int currentHealth { get; set; }
 
@@ -19,6 +27,7 @@ public class EnemyController : MonoBehaviour
         _rigidBody = GetComponent<Rigidbody>();
         currentHealth = _EnemyConfig.MaxHealth;
         currentState = States.Idle;
+        _summonGravityInversionFieldLock = false;
     }
 
     public void Update()
@@ -51,7 +60,29 @@ public class EnemyController : MonoBehaviour
 
     private void Summoning()
     {
-        Debug.Log("Summoning!");
+        if(!_summonGravityInversionFieldLock) SummonGravityInversionField();
+        if (!IsPlayerInSummoningRange()) currentState = States.Chasing;
+
+    }
+
+    private void SummonGravityInversionField()
+    {
+        // Fire a raycast to determine the Y axis of the gravity inversion field.
+        RaycastHit hit = new RaycastHit();
+        if (!Physics.Raycast(transform.position, Vector3.down, out hit)) return;
+        float gravityInversionFieldYAxis = hit.transform.position.y;
+        
+        // Determine the location of the gravity inversion field.
+        Vector3 gravityInversionFieldLocation = playerObject.transform.position;
+        gravityInversionFieldLocation.y = gravityInversionFieldYAxis + 14;
+        
+        GameObject gravityInversionField = Instantiate(gravityInversionFieldReference, 
+                                                       gravityInversionFieldLocation, 
+                                                       Quaternion.identity);
+        _summonGravityInversionFieldLock = true;
+        
+        StartCoroutine(SummonLockResetTimer());
+        StartCoroutine(DestroyAfterTime(gravityInversionField));
     }
 
     private bool IsPlayerInChasingRange()
@@ -77,5 +108,17 @@ public class EnemyController : MonoBehaviour
         Vector3 distanceTowardsPlayer = playerObject.transform.position - transform.position;
         Vector3 DirectiontowardsPlayer = distanceTowardsPlayer.normalized;
         _rigidBody.AddForce(DirectiontowardsPlayer * (Time.deltaTime * _EnemyConfig.movementForceFactor));
+    }
+    
+    private IEnumerator SummonLockResetTimer()
+    {
+        yield return new WaitForSeconds(_summoningReloadTime);
+        _summonGravityInversionFieldLock = false;
+    }
+    
+    private IEnumerator DestroyAfterTime(Object gameObject)
+    {
+        yield return new WaitForSeconds(gravityInversionFieldLifetime);
+        Destroy(gameObject);
     }
 }
